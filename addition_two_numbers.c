e21a00311bc56f60f7062068f847f1719fda3cd7
LuvongaW <168713095+LuvongaW@users.noreply.github.com>
2026-02-21 15:02:39 +0300

    Add files via upload

---
+++ addition_two_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+int a; //the first number
+int b; //the second number
+int sum; //addition
+printf("Enter the first number:\n");
+scanf("%d" ,&a);
+printf("Enter the second number\n");
+scanf("%d", &b);
+sum = a+b;
+printf("The sum is: %d", sum);
+return 0;
+
+
+
+}
+
+
+
+
+
+
